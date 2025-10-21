be5206e893a080755816842969efc77de900bb32
HaoNguyen2004-Iter <hao.d22c01a6023@ktkt.edu.vn>
2025-10-21 10:39:46 +0700

    Initial commit

---
+++ SPMH.Services/Executes/Storage/ImageStorage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPMH.Services.Models;
+
+namespace SPMH.Services.Executes.Storage
+{
+    public class ImageStorage
+    {
+        private static readonly HashSet<string> AllowedExt = new(StringComparer.OrdinalIgnoreCase)
+        {
+            ".jpg", ".jpeg", ".png", ".gif", ".webp"
+        };
+
+        private readonly string _physicalRoot;
+        private readonly string _publicBasePath;
+
+
+    }
+}
---
+++ SPMH.Services/Models/ProductImage.cs
@@ -0,0 +1,9 @@
+﻿namespace SPMH.Services.Models
+{
+    public record ProductImage
+    {
+        public required string Url { get; init; }
+        public string? Alt { get; init; }
+        public required string OriginalFileName { get; set; }
+    }
+}
